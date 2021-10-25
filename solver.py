917ec82fb7f9610ff38063d464eaa6108493291a
jarinsimon <jarin.simon@gmail.com>
2021-10-25 12:44:33 -0700

    validation check and solving algorithms

--- solver.py
+++ solver.py
@@ -34,8 +34,53 @@ def find_space(board):
 	return None
 
 
+
+def solver(board):
+	find = find_space(board)
+	if not find:
+		return True
+	else:
+		row, column = find
+	
+	for i in range(1,10):
+		if check_valid(board, i, (row, column)):
+			board[row][column] = i
+
+			if solver(board):
+				return True
+
+			board[row][column] = 0
+
+	return False
+
+def check_valid(board, number, position):
+	for i in range(len(board[0])):
+		if board[position[0]][i] == number and position[1] != i:
+			return False
+	
+	for i in range(len(board)):
+		if board[i][position[1]] == number and position[0] != i:
+			return False
+
+	box_x = position[1] // 3
+	box_y = position[0] // 3
+
+#	print(box_x)
+#	print(box_y)
+
+	for i in range(box_y * 3, box_y * 3 + 3):
+		for j in range(box_x * 3, box_x*3):
+			if board[i][j] == number and (i, j) != position:
+				return False
+	
+	return True
+
+
+
+
+print_board(board)
+solver(board)
 print("______________________")
 print_board(board)
-
 #def solver(board):
 	
